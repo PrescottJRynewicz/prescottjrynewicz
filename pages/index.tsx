55f8e1ecafb091e3e9da0910af6345fa2d49ebc1
Prescott Rynewicz <prescott@cameo.com>
2021-08-14 12:24:22 -0700

    Try out a change.

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to my personal website!</h1>
 
         <p className={styles.description}>
           Explore the example pages in the{' '}
