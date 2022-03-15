b8cb6ff60ef1e86daa08bd92d8fc1826f3c3086a
pjrynewicz <pjrynewicz@gmail.com>
2022-03-15 15:48:49 -0700

    Add static paths.

--- pages/blog/[...blogPost].tsx
+++ pages/blog/[...blogPost].tsx
@@ -1,10 +1,43 @@
 import { BlogPost, BlogPostProps } from '/src/pages/Blog/BlogPost';
-import { GetStaticPropsContext } from 'next';
+import { GetStaticPathsResult, GetStaticPropsContext } from 'next';
 import fetch from 'node-fetch';
 import { BlogPostGetResponse } from '/src/types/api/blog/posts';
 import { getApiUrl } from '/src/utils/url/getApiUrl';
+import { BlogGetResponse } from '/src/types/api/blog';
 
 export default BlogPost;
+
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+  const url = getApiUrl(`blog`);
+
+  console.log('here');
+  const rawResult = await fetch(url, {
+    method: 'POST',
+  });
+
+  const blogs: BlogGetResponse =
+    (await rawResult.json()) as unknown as BlogGetResponse;
+
+  console.log({
+    paths: blogs.posts.map((post) => ({
+      params: {
+        blogPost: [
+          post.properties.Title.title.map((item) => item.plain_text).join('-'),
+        ],
+      },
+    })),
+  });
+  return {
+    paths: blogs.posts.map((post) => ({
+      params: {
+        blogPost: [
+          post.properties.Title.title.map((item) => item.plain_text).join('-'),
+        ],
+      },
+    })),
+    fallback: true,
+  };
+}
 
 export async function getStaticProps(context: GetStaticPropsContext): Promise<{
   props: BlogPostProps;
