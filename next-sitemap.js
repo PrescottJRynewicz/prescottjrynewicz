df668177b36dcbdf0107d36ef4a447cd7a72b117
pjrynewicz <pjrynewicz@gmail.com>
2022-03-19 10:06:05 -0700

    Add sitemap.

---
+++ next-sitemap.js
@@ -0,0 +1,34 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_WEBSITE_URL || 'https://www.prescottjr.com',
+  changefreq: 'hourly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  exclude: [],
+  // Default transformation function
+  transform: async (config, path) => ({
+    loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+    changefreq: config.changefreq,
+    priority: config.priority,
+    lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    alternateRefs: config.alternateRefs ?? [],
+  }),
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot-image',
+        allow: '/',
+      },
+    ],
+  },
+};
---
+++ pages/sitemap.xml/index.tsx
@@ -0,0 +1,29 @@
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
+import { GetServerSideProps } from 'next';
+import { getApiUrl } from '/src/utils/url/getApiUrl';
+import fetch from 'node-fetch';
+import { BlogGetResponse } from '/src/types/api/blog';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const url = getApiUrl(`blog`);
+
+  const rawResult = await fetch(`${url}`, {
+    method: 'POST',
+  });
+
+  const blogPosts: BlogGetResponse =
+    (await rawResult.json()) as unknown as BlogGetResponse;
+
+  const routes: ISitemapField[] = blogPosts.posts.map((post) => ({
+    loc: `https://prescottjr.com/${post.properties.Title.title
+      .map((item) => item.plain_text)
+      .join()
+      .replace(/\s/g, '-')}`,
+    lastmod: new Date().toDateString(),
+    priority: 0.7,
+  }));
+
+  return getServerSideSitemap(ctx, routes);
+};
+
+export default function Sitemap() {}
