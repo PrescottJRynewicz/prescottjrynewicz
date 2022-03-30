b96401ac80590ca14eca05be1484854877e0bcd5
pjrynewicz <pjrynewicz@gmail.com>
2022-03-29 23:08:35 -0700

    Extract image cache method.

--- pages/blog/[...blogPost].tsx
+++ pages/blog/[...blogPost].tsx
@@ -6,16 +6,7 @@ import {
 } from 'next';
 import { getBlogPosts } from '/src/fetchers/getBlogPosts';
 import { getBlogPost } from '/src/fetchers/getPost';
-import {
-  getPreviewImage,
-  getPreviewImageMap,
-} from '/src/utils/getPreviewImages';
-import { speckles } from '/design-system/colors';
-import { PageCover } from '/src/types/cms/properties';
-import { normalizeUrl } from 'notion-utils';
-import { PreviewImage } from 'notion-types';
-import { getImageCache } from '/src/fetchers/getImageCache';
-import { setImageCache } from '/src/fetchers/setImageCache';
+import { imageBlurCaching } from '/src/fetchers/imageBlugCaching';
 
 export default BlogPost;
 
@@ -55,48 +46,12 @@ export async function getStaticProps(
 
     const { post, pageData } = blogPost;
 
-    let cache: Record<string, PreviewImage> = {};
-
-    const { combinedText, blockIds } = await getImageCache(pageData.id);
-    try {
-      cache = JSON.parse(combinedText);
-    } catch (err) {
-      cache = {};
-    }
-
-    const { previewImagesMap, newCache } = await getPreviewImageMap(
+    const { previewImagesMap, coverPreview } = await imageBlurCaching({
       post,
-      cache
-    );
-
-    const coverPhoto = pageData.cover as PageCover;
-    const { url } = coverPhoto?.[coverPhoto?.type] || {
-      url: speckles.MILK,
-    };
-
-    const coverCacheKey = normalizeUrl(url);
-    const coverPreview = await getPreviewImage(
-      url,
-      { cacheKey: coverCacheKey },
-      cache
-    );
-
-    newCache[coverCacheKey] = coverPreview as PreviewImage;
+      pageData,
+    });
 
     post.preview_images = previewImagesMap;
-
-    const newCacheString = Object.entries(newCache)
-      .map((value) => value)
-      .sort()
-      .toString();
-    const oldCacheString = Object.entries(cache)
-      .map((value) => value)
-      .sort()
-      .toString();
-
-    if (oldCacheString !== newCacheString) {
-      setImageCache(newCache, blockIds, pageData.id).catch(console.error);
-    }
 
     return {
       props: {
