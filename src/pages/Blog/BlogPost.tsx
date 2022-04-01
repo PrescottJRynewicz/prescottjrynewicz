9d3f533430c8127879ea32e4709d8e002a292347
pjrynewicz <pjrynewicz@gmail.com>
2022-04-01 09:49:33 -0700

    Add emoji as favicon

--- src/pages/Blog/BlogPost.tsx
+++ src/pages/Blog/BlogPost.tsx
@@ -113,7 +113,10 @@ export function BlogPost({ post, pageData, coverBlurUrl }: BlogPostProps) {
           name="description"
           content={subtitle.rich_text.map((item) => item.plain_text).join()}
         />
-        <link rel="icon" href="/favicon.png" />
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%22100%22>${icon?.emoji}</text></svg>`}
+        />
 
         <meta property="og:image" content={url} />
         <meta
