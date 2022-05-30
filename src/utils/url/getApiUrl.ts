04f89482af376551f9e7eeb17b14173bf7a4fe1c
pjrynewicz <pjrynewicz@gmail.com>
2022-05-30 16:18:49 -0700

    Let's try this

--- src/utils/url/getApiUrl.ts
+++ src/utils/url/getApiUrl.ts
@@ -7,7 +7,9 @@ export function getUrl(path?: string) {
 export function getApiUrl(path?: string) {
   console.log(process.env.NEXT_PUBLIC_VERCEL_ENV);
   console.log(process.env.NEXT_PUBLIC_VERCEL_URL, '\n\n');
-  return process.env.NODE_ENV === 'development'
+  return process.env.NEXT_PUBLIC_VERCEL_ENV === 'development'
     ? `http://localhost:3000/api/${path || ''}`
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/${path || ''}`
     : `${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/${path || ''}`;
 }
