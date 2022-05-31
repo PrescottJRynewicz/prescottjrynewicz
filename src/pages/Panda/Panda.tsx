205d06d573611ef3dcb2bdafbd5e98db39c915dd
pjrynewicz <pjrynewicz@gmail.com>
2022-05-30 18:09:37 -0700

    Add border.

--- src/pages/Panda/Panda.tsx
+++ src/pages/Panda/Panda.tsx
@@ -3,13 +3,18 @@ import { Core } from 'cytoscape';
 import { useGraph } from '/src/pages/Panda/effects/useGraph';
 import { GraphCache, UserEntry } from '/src/fetchers/panda/constants';
 import styled from 'styled-components';
+import { solids } from '/design-system/colors';
 
 export type PandaProps = {
   panda: UserEntry;
   graph: GraphCache;
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  border: solid 7px ${solids.PINK_STARBURST};
+  width: 100vw;
+  height: 100vh;
+`;
 
 export function Panda({ panda, graph }: PandaProps) {
   const graphRef = useRef<HTMLDivElement>();
@@ -24,8 +29,8 @@ export function Panda({ panda, graph }: PandaProps) {
         ref={graphRef as MutableRefObject<HTMLDivElement>}
         style={{
           backgroundColor: 'transparent',
-          width: '100vw',
-          height: '100vh',
+          width: '100%',
+          height: '100%',
         }}
       />
     </Container>
