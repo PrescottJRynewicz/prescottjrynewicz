f0895d9efb43a7b5766040096a4ce001198be348
pjrynewicz <pjrynewicz@gmail.com>
2022-06-02 12:41:36 -0700

    Remove unecessary files.

--- src/fetchers/panda/buildUrl.ts
+++
@@ -1,7 +0,0 @@
-export function buildUrl(
-  userId: string,
-  followType: 'following' | 'followers',
-  page: number
-) {
-  return `https://www.strava.com/athletes/${userId}/follows?page=${page}&page_uses_modern_javascript=true&type=${followType}`;
-}
