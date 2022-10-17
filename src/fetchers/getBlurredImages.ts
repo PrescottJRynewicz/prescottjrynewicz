37c06ac5e34533dc1336057824f7f0881bda9ad1
pjrynewicz <pjrynewicz@gmail.com>
2022-10-16 22:02:16 -0700

    Remove image cache and update next + notion client

--- src/fetchers/getBlurredImages.ts
+++ src/fetchers/getBlurredImages.ts
@@ -1,13 +1,10 @@
-import { ExtendedRecordMap, PreviewImage } from 'notion-types';
-import { getImageCache } from '/src/fetchers/getImageCache';
+import { ExtendedRecordMap } from 'notion-types';
 import {
   getPreviewImage,
   getPreviewImageMap,
 } from '/src/utils/getPreviewImages';
 import { NotionPage, PageCover } from '/src/types/cms/properties';
 import { speckles } from '/design-system/colors';
-import { normalizeUrl } from 'notion-utils';
-import { setImageCache } from '/src/fetchers/setImageCache';
 
 /**
  * This method will take a post, and attach image previews.
@@ -24,37 +21,16 @@ export async function getBlurredImages({
   post: ExtendedRecordMap;
   pageData: NotionPage;
 }) {
-  const { cache, blockIds } = await getImageCache(pageData);
+  // const { cache, blockIds } = await getImageCache(pageData);
 
-  const { previewImagesMap, newCache } = await getPreviewImageMap(post, cache);
+  const { previewImagesMap } = await getPreviewImageMap(post);
 
   const coverPhoto = pageData.cover as PageCover;
   const { url } = coverPhoto?.[coverPhoto?.type] || {
     url: speckles.MILK,
   };
 
-  const coverCacheKey = normalizeUrl(url);
-  const coverPreview = await getPreviewImage(
-    url,
-    { cacheKey: coverCacheKey },
-    cache
-  );
-
-  newCache[coverCacheKey] = coverPreview as PreviewImage;
-
-  const newCacheString = Object.entries(newCache)
-    .map((value) => value)
-    .sort()
-    .toString();
-  const oldCacheString = Object.entries(cache)
-    .map((value) => value)
-    .sort()
-    .toString();
-
-  if (oldCacheString !== newCacheString) {
-    console.log('updating image cache for page', pageData.id);
-    setImageCache(newCache, blockIds, pageData).catch(console.error);
-  }
+  const coverPreview = await getPreviewImage(url);
 
   return {
     previewImagesMap,
