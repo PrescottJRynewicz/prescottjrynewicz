6f3000481837fbdaf82a90cd97a3d841dcf92b9c
Prescott Rynewicz <pjrynewicz@gmail.com>
2024-07-11 20:39:18 +0000

    FIx URL structure

--- src/fetchers/getPost.ts
+++ src/fetchers/getPost.ts
@@ -3,6 +3,7 @@ import { NotionPage } from '/src/types/cms/properties';
 import { NotionAPI } from 'notion-client';
 import { Client } from '@notionhq/client';
 
+const blogDatbaseId = process.env.BLOG_DATABASE_ID;
 const notionAPIKey = process.env.NOTION_API_KEY;
 const notionUserTokenV2 = process.env.NOTION_USER_SECRET;
 
@@ -28,6 +29,29 @@ export async function getBlogPost({
   const pageData = pageResult as NotionPage;
 
   if (!pageData) {
+    // attempt old URL structure
+    const postName = (name as string).replace(/-/g, ' ').toLowerCase();
+
+    const postQuery = await notion.databases.query({
+      database_id: blogDatbaseId as string,
+      filter: {
+        property: 'title',
+        rich_text: {
+          equals: postName,
+        },
+      },
+    });
+
+    const pageData = postQuery.results[0] as NotionPage;
+    if (pageData) {
+      const post = await api.getPage(pageData.id);
+
+      return {
+        post,
+        pageData,
+      };
+    }
+
     throw Error('Unable to find post');
   }
 
